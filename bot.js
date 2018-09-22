50c730cfe27137c9adcdc45d6247f382b521a6f2
barbar321123 <43475649+barbar321123@users.noreply.github.com>
2018-09-22 08:43:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.reply('Sunucunun Ping Değeri!' + client.ping + ' ms');
+    	message.reply('Sunucunun Ping Değeri! ' + client.ping + ' ms');
   	}
 });
 
