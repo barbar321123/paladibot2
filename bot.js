820ba2065105ec83296eed3f86b87953795a9634
barbar321123 <43475649+barbar321123@users.noreply.github.com>
2018-09-22 08:50:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,35 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
-
 client.on('ready', () => {
-  console.log(`[BOT] ${client.user.tag} ADI İLE GİRİŞ YAPTI!`);
+    console.log('BOT {client.user.tag} ADI İLE GİRİŞ YAPTI!');
 });
 
 client.on('message', message => {
-  if (message.content === 'ping') {
-    message.channel.send('Pong!' + client.ping + 'ms');
-  }
-  
-    if (message.content === 'sa') {
-    message.reply('as');
-  }
-  
-  if (message.content === prefix + 'avatarım') {  
-	message.channel.send(message.author.avatarURL);
-  }
-  
-  if (message.content === prefix + 'reboot') {
-	  
-	  if (message.author.id === "muhteşim id niz") {
-		  message.channel.send('[BOT]Yenden başlatılıyor...').then(msg => {
-			  console.log('yeniden başlatılıyor')
-			  process.exit(0);
-		  });
-	  } else 
-		  message.channel.send('Maalesef bu komutu kullanamazsın. Benim yapımcım değilsin!')
-  }
-  
+    if (message.content === 'ping') {
+    	message.reply('Sunucunun Ping Değeri! ' + client.ping + ' ms');
+  	}
   
 });
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
