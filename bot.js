2751393d58dc27f152ffcfed52910d2d30eabe93
barbar321123 <43475649+barbar321123@users.noreply.github.com>
2018-10-07 11:59:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,17 @@ client.on('message', message => {
         if (message.content === prefix + 'davetlinki') {
           message.reply('https://discordapp.com/oauth2/authorize?client_id=492398392253612073&scope=bot&permissions=2146958847');
         }
- }
+        if (message.content === prefix + 'reboot') {
+        
+        if (message.author.id === '492951512730042369') {
+            message.channel.send('[BOT] Yeniden Başlatılıyor...').then (msg => {
+                console.log('Bot Yeniden Başlıyor');
+            process.exit(0)
+            });
+        }else {
+            message.channel.send('Bu Yetkiye Sahip Değilsiniz!!');
+        }
+    }
 
 });
 
